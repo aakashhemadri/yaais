1ca93b2a1ef5b0202c698eb52be1b676bfe1b9ce
Aakash Hemadri <aakashhemadri123@gmail.com>
2020-01-30 12:23:07 +0530

    Add uname

--- install.sh
+++ install.sh
@@ -3,4 +3,16 @@ export AUR_MGR="yay"
 
 declare -r GITCLONE="git clone --depth=1"
 
-if [ -z "$HOME" ]; then echo "Seems you're \$HOMEless :("; exit 1; fi
+SYSTEM_TYPE=$(uname -s)
+OS=$(uname -o)
+
+if [ "${SYSTEM_TYPE}" = "Linux" ]; then
+    echo -e "Starting yaais.. for ${OS}";
+		echo -e "Running checks..."
+		if [ -z "$HOME" ]; then 
+			echo "Seems you're \$HOMEless :("; 
+			echo -e "\$HOME - done";
+			exit 1; 
+		fi
+
+fi
